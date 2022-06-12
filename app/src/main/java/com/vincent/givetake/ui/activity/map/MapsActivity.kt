752ce4eba2cf6787d9c5c4dda3893acce20ac0a1
vincentjordan27 <vincentiustampubolon2@gmail.com>
2022-06-12 14:59:58 +0700

    Implement MyLocation

--- app/src/main/java/com/vincent/givetake/ui/activity/map/MapsActivity.kt
+++ app/src/main/java/com/vincent/givetake/ui/activity/map/MapsActivity.kt
@@ -1,11 +1,21 @@
 package com.vincent.givetake.ui.activity.map
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.google.android.gms.common.api.Status
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -20,7 +30,6 @@ import com.google.android.libraries.places.widget.listener.PlaceSelectionListene
 import com.google.android.material.snackbar.Snackbar
 import com.vincent.givetake.R
 import com.vincent.givetake.databinding.ActivityMapsBinding
-import kotlinx.android.synthetic.main.activity_register.*
 import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -28,6 +37,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private var data: AddressResult? = null
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var geocoder: Geocoder
+    private lateinit var autocompleteFragment: AutocompleteSupportFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,12 +65,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 finish()
             }
         }
+
+        binding.btnMyLocation.setOnClickListener {
+            getMyLastLocation()
+        }
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        geocoder = Geocoder(this, Locale.getDefault())
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        val autocompleteFragment =
+        autocompleteFragment =
             supportFragmentManager.findFragmentById(R.id.autocomplete_fragment)
                     as AutocompleteSupportFragment
 
@@ -81,7 +100,78 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
 
         })
+    }
 
+    private fun checkPermission(permission: String) : Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
 
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions[Manifest.permission.ACCESS_FINE_LOCATION] ?: false -> {
+                getMyLastLocation()
+            }
+            permissions[Manifest.permission.ACCESS_COARSE_LOCATION] ?: false -> {
+                getMyLastLocation()
+            }
+            else -> {
+                showToast("Akses lokasi tidak diberikan")
+            }
+        }
+    }
+
+    private fun getMyLastLocation() {
+        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION) && checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            showLoading(true)
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    showMarkerMyLocation(location)
+                } else {
+                    showToast("Lokasi tidak ditemukan. Coba lagi")
+                }
+            }
+        }else {
+            requestPermissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showMarkerMyLocation(location: Location) {
+        showLoading(false)
+        val myLocation = LatLng(location.latitude, location.longitude)
+        val geocoder = geocoder.getFromLocation(location.latitude, location.longitude, 1)
+        data = AddressResult(
+            myLocation,
+            geocoder[0].featureName,
+            geocoder[0].getAddressLine(0)
+        )
+        mMap.addMarker(
+            MarkerOptions()
+                .position(myLocation)
+                .title("Lokasi Saya")
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 17F))
+        autocompleteFragment.setText("")
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.pg.visibility = View.VISIBLE
+        } else {
+            binding.pg.visibility = View.GONE
+        }
     }
 }
